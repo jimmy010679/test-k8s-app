a02f40b5d9ba0efabb17d57e5aa84b7f8d4b51d5
jimmy010679 <jimmy010679@gmail.com>
2026-05-12 04:26:03 +0800

    test: sampler

--- src/instrumentation.node.ts
+++ src/instrumentation.node.ts
@@ -1,4 +1,3 @@
-import { IncomingMessage } from 'http';
 import { NodeSDK } from '@opentelemetry/sdk-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { resourceFromAttributes } from '@opentelemetry/resources';
@@ -6,11 +5,31 @@ import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { CompositePropagator, W3CTraceContextPropagator, W3CBaggagePropagator } from '@opentelemetry/core';
 import { CloudPropagator } from '@google-cloud/opentelemetry-cloud-trace-propagator';
-import { TraceIdRatioBasedSampler } from '@opentelemetry/sdk-trace-base';
+import { ParentBasedSampler, TraceIdRatioBasedSampler, Sampler, SamplingDecision,  SamplingResult } from '@opentelemetry/sdk-trace-base';
+import { Context, Attributes, SpanKind, Link } from '@opentelemetry/api';
 
 const otlpEndpoint = process.env.OTEL_EXPORTER_OTLP_ENDPOINT;
+const sampleRateStr = process.env.TRACE_SAMPLE_RATE;
+const sampleRate = sampleRateStr ? parseFloat(sampleRateStr) : 0.05;
+
+class RouteFilterSampler implements Sampler {
+  private baseSampler: Sampler;
+  constructor(baseSampler: Sampler) { this.baseSampler = baseSampler; }
+
+  shouldSample(context: Context, traceId: string, spanName: string, spanKind: SpanKind, attributes: Attributes, links: Link[]): SamplingResult {
+    const url = (attributes?.['http.url'] || attributes?.['http.target'] || spanName) as string;
+    
+    if (url && (url.includes('/healthz') || url.includes('/metrics') || url.includes('/_next'))) {
+      return { decision: SamplingDecision.NOT_RECORD };
+    }
+    return this.baseSampler.shouldSample(context, traceId, spanName, spanKind, attributes, links);
+  }
+  toString(): string { return `RouteFilterSampler`; }
+}
 
 if (otlpEndpoint) {
+  const baseRateSampler = new RouteFilterSampler(new TraceIdRatioBasedSampler(sampleRate));
+
   const sdk = new NodeSDK({
     // 定義這個服務的名稱，你在 Cloud Trace 畫面上會看到這個名字
     resource: resourceFromAttributes({
@@ -37,26 +56,16 @@ if (otlpEndpoint) {
       ],
     }),
 
-    // 採樣率 5%
-    sampler: new TraceIdRatioBasedSampler(0.05),
+    // sampler: new TraceIdRatioBasedSampler(0.05),
+    sampler: new ParentBasedSampler({
+      root: baseRateSampler, // 採樣率 5%       
+      remoteParentNotSampled: baseRateSampler, // 無視 LB，自己抽樣 1%~5%
+      remoteParentSampled: baseRateSampler,    
+    }),
 
     // 自動掛載常用的 Node.js 追蹤 (如 HTTP 請求、資料庫查詢等)
     instrumentations: [
-      getNodeAutoInstrumentations({
-        '@opentelemetry/instrumentation-http': {
-          // 忽略特定的請求，回傳 true 代表「不要追蹤」
-          ignoreIncomingRequestHook: (req: IncomingMessage) => {
-            if (!req.url) return false;
-            
-            // 排除路徑
-            const isHealthCheck = req.url.startsWith('/healthz');
-            const isMetrics = req.url.startsWith('/metrics');
-            const isNextStatic = req.url.startsWith('/_next');
-            
-            return isHealthCheck || isMetrics || isNextStatic;
-          },
-        },
-      })
+      getNodeAutoInstrumentations()
     ],
   });
 
